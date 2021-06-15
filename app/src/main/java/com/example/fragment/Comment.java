433bea7a1878bfbef9cbf3e6daecf5a5f2b91060
nassim rayene zorgani <rn.zorgani@esi-sba.dz>
2021-06-15 14:44:41 +0100

    nassim finished his part

--- app/src/main/java/com/example/fragment/Comment.java
+++ app/src/main/java/com/example/fragment/Comment.java
@@ -3,62 +3,32 @@ package com.example.fragment;
 import com.google.firebase.database.ServerValue;
 
 public class Comment {
-    private String content, uid, uimg, uname,key;
+    private String content,  uname,key, reply;
     private float rating;
+
     private Object timestamp;
 
     public Comment() {
     }
 
-    public Comment(String content, String uid, String uimg, String uname, float rating) {
+    public Comment(String content, String uname,float rating) {
         this.content = content;
-        this.uid = uid;
-        this.uimg = uimg;
         this.uname = uname;
         this.rating = rating;
 
         this.timestamp = ServerValue.TIMESTAMP;
     }
 
-    public Comment(String content, String uid, String uimg, String uname, float rating, Object timestamp) {
-        this.content = content;
-        this.uid = uid;
-        this.uimg = uimg;
-        this.uname = uname;
+    public String getReply() {
+        return reply;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
         this.rating = rating;
-        this.timestamp = timestamp;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getUid() {
-        return uid;
-    }
-
-    public void setUid(String uid) {
-        this.uid = uid;
-    }
-
-    public String getUimg() {
-        return uimg;
-    }
-
-    public void setUimg(String uimg) {
-        this.uimg = uimg;
-    }
-
-    public String getUname() {
-        return uname;
-    }
-
-    public void setUname(String uname) {
-        this.uname = uname;
     }
 
     public String getKey() {
@@ -69,12 +39,29 @@ public class Comment {
         this.key = key;
     }
 
-    public float getRating() {
-        return rating;
+    public String getContent() {
+        return content;
     }
 
-    public void setRating(float rating) {
-        this.rating = rating;
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+
+
+
+    public String getUname() {
+        return uname;
+    }
+
+
+    public void setReply(String reply) {
+        this.reply = reply;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
     }
 
     public Object getTimestamp() {
@@ -89,8 +76,7 @@ public class Comment {
     public String toString() {
         return "Comment{" +
                 "content='" + content + '\'' +
-                ", uid='" + uid + '\'' +
-                ", uimg='" + uimg + '\'' +
+
                 ", uname='" + uname + '\'' +
                 ", key='" + key + '\'' +
                 ", rating=" + rating +
--- app/src/main/java/com/example/fragment/CommentAdapter.java
+++ app/src/main/java/com/example/fragment/CommentAdapter.java
@@ -1,10 +1,14 @@
 package com.example.fragment;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.text.format.DateFormat;
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -62,7 +66,30 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentV
                 notifyItemRangeChanged(position, mData.size());
             }
         });
+        holder.replyComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(mContext,R.style.Theme_AppCompat_Light_Dialog_Alert));
+                final View replyLayout = LayoutInflater.from(mContext).inflate(R.layout.add_reply,null);
+                builder.setView(replyLayout);
+                builder.setTitle("Admin reply");
+                EditText commentReply = replyLayout.findViewById(R.id.comment_reply_edittext);
+                commentReply.setText(mData.get(position).getReply());
 
+                builder.setPositiveButton("SUBMIT", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        DatabaseReference commentRef = FirebaseDatabase.getInstance().getReference().child(Reviews.COMMENT_KEY).child(mData.get(position).getKey()).child("reply");
+                        commentRef.setValue(commentReply.getText().toString());
+
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+            }
+        });
     }
 
     @Override
@@ -71,7 +98,7 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentV
     }
 
     public class CommentViewHolder extends RecyclerView.ViewHolder{
-        ImageView userPhoto,removeComment;
+        ImageView userPhoto,removeComment,replyComment;
         TextView name,content,date;
         RatingBar commentRatingShow;
 
@@ -83,6 +110,7 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentV
             date = itemView.findViewById(R.id.comment_date);
             commentRatingShow = itemView.findViewById(R.id.comment_ratingbar);
             removeComment = itemView.findViewById(R.id.comment_remove);
+            replyComment = itemView.findViewById(R.id.comment_add_reply);
         }
     }
     private void getUserPhoto(@NonNull CommentViewHolder holder, int position) {
